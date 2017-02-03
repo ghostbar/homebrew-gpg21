f3fbd202af5205e492dee79a57ec862d796da1b2
Jose-Luis Rivas <me@ghostbar.co>
2017-02-03 18:34:39 -0500

    feat(gpg21): upgrade to 2.1.18, no need for dirmngr patches

--- gpg21.rb
+++ gpg21.rb
@@ -1,17 +1,9 @@
 class Gpg21 < Formula
   desc "GNU Privacy Guard: a free PGP replacement"
   homepage "https://www.gnupg.org/"
-  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.17.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.17.tar.bz2"
-  sha256 "c5dc54db432209fa8f9bdb071c8fb60a765ff28e363150e30bdd4543160243cb"
-
-  bottle do
-    sha256 "54c56057f77131999c28c36b8f1c7634cc49d93547c52e979afcffb18c5e414d" => :sierra
-    sha256 "3318380b33d6f8b96219885d5e633214160a91e067f4914c17c8755bc1910afb" => :el_capitan
-    sha256 "aaafb15c59f8d81683a2122ad9d7bbf9df15b38ebf6f4b0ac416d2f06e044d05" => :yosemite
-  end
-
-  patch :DATA
+  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.1.18.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gnupg/gnupg-2.1.18.tar.bz2"
+  sha256 "d04c6fab7e5562ce4b915b22020e34d4c1a256847690cf149842264fc7cef994"
 
   option "with-gpgsplit", "Additionally install the gpgsplit utility"
   option "without-libusb", "Disable the internal CCID driver"
@@ -122,51 +114,3 @@ class Gpg21 < Formula
     system bin/"gpgconf"
   end
 end
-
-__END__
-diff --git a/dirmngr/dns-stuff.c b/dirmngr/dns-stuff.c
---- a/dirmngr/dns-stuff.c
-+++ b/dirmngr/dns-stuff.c
-@@ -478,7 +478,16 @@ libdns_init (void)
-       if (err)
-         {
-           log_error ("failed to load '%s': %s\n", fname, gpg_strerror (err));
--          goto leave;
-+          /* not fatal, nsswitch.conf is not used on all systems; assume
-+           * classic behavior instead.  Our dns library states "bf" which tries
-+           * DNS then Files, which is not classic; FreeBSD
-+           * /usr/src/lib/libc/net/gethostnamadr.c defines default_src[] which
-+           * is Files then DNS, which is. */
-+          log_debug ("dns: fallback resolution order, files then DNS");
-+          ld.resolv_conf->lookup[0] = 'f';
-+          ld.resolv_conf->lookup[1] = 'b';
-+          ld.resolv_conf->lookup[2] = '\0';
-+          err = GPG_ERR_NO_ERROR;
-         }
- 
- #endif /* Unix */
-@@ -732,6 +732,10 @@ resolve_name_libdns (const char *name, unsigned short port,
-               err = gpg_error_from_syserror ();
-               goto leave;
-             }
-+          /* Libdns appends the root zone part which is problematic
-+           * for most other functions - strip it.  */
-+          if (**r_canonname && (*r_canonname)[strlen (*r_canonname)-1] == '.')
-+            (*r_canonname)[strlen (*r_canonname)-1] = 0;
-         }
- 
-       dai = xtrymalloc (sizeof *dai + ent->ai_addrlen -1);
-@@ -1899,6 +1903,13 @@ get_dns_cname_libdns (const char *name, char **r_cname)
-   *r_cname = xtrystrdup (cname.host);
-   if (!*r_cname)
-     err = gpg_error_from_syserror ();
-+  else
-+    {
-+      /* Libdns appends the root zone part which is problematic
-+       * for most other functions - strip it.  */
-+      if (**r_cname && (*r_cname)[strlen (*r_cname)-1] == '.')
-+        (*r_cname)[strlen (*r_cname)-1] = 0;
-+    }
- 
-  leave:
-   dns_free (ans);
